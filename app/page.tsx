b5c17e168cdb7a34180ae9709d45b5424bc0582e
NathanDinh <nathan.dinh03@outlook.com>
2025-11-27 21:09:35 -0500

    feat: Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { getLandingPage } from '@/lib/contentful'
+import { getLandingPage } from "@/lib/contentful";
+import HeroSection from "./components/market/HeroSection";
 
 export default async function Home() {
-    const data = await getLandingPage()
+  const data = await getLandingPage();
 
-    return (
-        <div className="p-8">
-            <h1 className="text-4xl font-bold">{data.fields.heroHeadline}</h1>
-            <p className="text-xl mt-4">{data.fields.heroSubheadline}</p>
-            <pre className="mt-8 bg-black text-white p-4 rounded">
-        {JSON.stringify(data.fields, null, 2)}
-      </pre>
-        </div>
-    )
+  return (
+    <>
+      <HeroSection />
+      <h1 className="text-4xl font-bold">{data.fields.heroHeadline}</h1>
+      <p className="text-xl mt-4">{data.fields.heroSubheadline}</p>
+      <pre className="mt-8 bg-black text-white p-4 rounded">{JSON.stringify(data.fields, null, 2)}</pre>
+    </>
+  );
 }
