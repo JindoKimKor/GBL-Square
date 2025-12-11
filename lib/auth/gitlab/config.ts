0c1ee50af16efed5a795778672440eec1149bd01
Juhwan21st <jseo9123@conestogac.on.ca>
2025-12-11 14:19:16 -0500

    feat: add GitLab OAuth configuration (Story #47, Task #56)
    Registered GitLab OAuth application and set up configuration module
    for GitLab authentication integration.
    
    Changes:
    - Created lib/auth/gitlab/config.ts with OAuth endpoints and settings
    - Added GITLAB_CLIENT_ID and GITLAB_CLIENT_SECRET to .env
    - Documented GitLab OAuth setup process in README
    
    GitLab OAuth app registered at gitlab.com/-/profile/applications with
    callback URL: http://localhost:3000/api/auth/gitlab/callback

---
+++ lib/auth/gitlab/config.ts
@@ -0,0 +1,24 @@
+/**
+ * GitLab OAuth Configuration
+ *
+ * Reference Documentation:
+ * - OAuth Flow: https://docs.gitlab.com/ee/api/oauth2.html
+ * - Scopes: https://docs.gitlab.com/ee/integration/oauth_provider.html#authorized-applications
+ * - User API: https://docs.gitlab.com/ee/api/users.html#for-user
+ */
+export const gitlabOAuthConfig = {
+  clientId: process.env.GITLAB_CLIENT_ID!,
+  clientSecret: process.env.GITLAB_CLIENT_SECRET!,
+
+  // GitLab OAuth URLs
+  authorizationUrl: "https://gitlab.com/oauth/authorize",
+  tokenUrl: "https://gitlab.com/oauth/token",
+  userApiUrl: "https://gitlab.com/api/v4/user",
+
+  // OAuth settings
+  scope: "read_user",
+
+  // Callback URL
+  redirectUri: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/gitlab/callback`,
+};
+
